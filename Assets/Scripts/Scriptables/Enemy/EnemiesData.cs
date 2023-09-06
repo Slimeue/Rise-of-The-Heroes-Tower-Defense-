755894118006ff57488d2f0d3653752f96f5ed93
Kevinroy <77309706+Slimeue@users.noreply.github.com>
2023-09-07 01:23:16 +0800

    Update Enemy and Character Data

--- Assets/Scripts/Scriptables/Enemy/EnemiesData.cs
+++ Assets/Scripts/Scriptables/Enemy/EnemiesData.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Enemies", menuName = "Enemy/Enemy Data")]
 public class EnemiesData : ScriptableObject
 {
-
+    [Space(5)]
     [SerializeField] public GameObject enemyPrefab;
+
+    [Space(5)]
+    [SerializeField] public float dmgValue;
+
+    [Space(5)]
+    [Header("Health")]
     [SerializeField] public float hitPoints;
     [SerializeField] public float maxHp;
+
+    [Space(5)]
+    [Header("Defense")]
+    [SerializeField] public float baseArmor;
+
 
 }
