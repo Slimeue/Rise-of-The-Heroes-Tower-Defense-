853bfa3525e98b7254841244dd1c167c1d6be125
Kevinroy <77309706+Slimeue@users.noreply.github.com>
2023-09-17 12:39:41 +0800

    Testing Touch

--- Assets/Scripts/Tower/TowerPlacement.cs
+++ Assets/Scripts/Tower/TowerPlacement.cs
@@ -6,15 +6,16 @@ using Cinemachine;
 
 public class TowerPlacement : MonoBehaviour
 {
-    PlayerInput playerInput;
+    //References
 
-    private InputAction _touchPressedAction;
-    private InputAction _touchHoldAction;
-
+    public GameObject[] _meleePlatform;
+    CanvasEnabler canvasEnabler;
+    //
     [Space(5)]
     [Header("Canvas")]
     [SerializeField] GameObject _CharConfirmPlacementCanvas;
     [SerializeField] GameObject _charInfoCanvas;
+    [SerializeField] string _platformTag;
 
     float placingGameTimeSpeed = 0.1f;
     float normalGameTimeSpeed = 1f;
@@ -31,9 +32,15 @@ public class TowerPlacement : MonoBehaviour
     [Header("Object Material")]
     [SerializeField] Material[] _materia;
 
+    [Space(5)]
+    [Header("Raycast")]
+    [SerializeField] LayerMask _layerPlatform;
+    float maxDistance;
 
     [SerializeField] CinemachineVirtualCamera vcam1;
     [SerializeField] CinemachineVirtualCamera vcam2;
+
+    bool rayCastHit;
 
     Renderer rend;
 
@@ -41,10 +48,10 @@ public class TowerPlacement : MonoBehaviour
 
     // Material mat;
 
-    GameObject _instPreviewObj;
-    GameObject _instObj;
+    private GameObject _instPreviewObj;
+    private GameObject _instObj;
 
-    public enum State
+    private enum State
     {
         Default,
         Placing,
@@ -57,17 +64,10 @@ public class TowerPlacement : MonoBehaviour
 
     private void Awake()
     {
-        playerInput = GetComponent<PlayerInput>();
-        _touchPressedAction = playerInput.actions["Pressed"];
-        _touchHoldAction = playerInput.actions["Hold"];
+
         currentState = State.Default;
-        // _touchPressedAction.canceled += OnHoldCanceled;
-    }
 
-
-
-    private void Start()
-    {
+        _meleePlatform = GameObject.FindGameObjectsWithTag(_platformTag);
 
     }
 
@@ -78,16 +78,18 @@ public class TowerPlacement : MonoBehaviour
         {
             case State.Default:
 
+
                 Time.timeScale = normalGameTimeSpeed;
                 vcam1.Priority = 1;
                 vcam2.Priority = 0;
                 Debug.Log(currentState);
                 _charInfoCanvas.SetActive(false);
+                DisablePlatformCanvas();
                 break;
             case State.Placing:
-                _touchPressedAction.performed += OnPlacing;
-                _touchPressedAction.canceled += Hold;
                 Time.timeScale = placingGameTimeSpeed;
+                OnPlacing();
+                EnablePlatformCanvas();
                 //TODO:: Move the camera a bit when palcing
                 vcam1.Priority = 0;
                 vcam2.Priority = 1;
@@ -95,25 +97,35 @@ public class TowerPlacement : MonoBehaviour
                 _charInfoCanvas.SetActive(true);
                 break;
             case State.HoldPlacing:
-
+                Debug.Log(currentState);
+                _CharConfirmPlacementCanvas.SetActive(true);
+                _CharConfirmPlacementCanvas.transform.position = _instPreviewObj.transform.position;
 
                 break;
             case State.Placed:
-
+                Debug.Log(currentState);
 
                 break;
         }
 
-
     }
 
+    private void OnEnable()
+    {
+
+    }
     private void OnDisable()
     {
-        _touchHoldAction.performed -= OnPlacing;
+
+
     }
 
     public void OnPressed()
     {
+
+
+        Debug.Log("Game object name: !" + gameObject.name);
+        Debug.Log("name of prefab: " + _tower.name);
         if (_isPlacing)
         {
             _isPlacing = false;
@@ -127,58 +139,87 @@ public class TowerPlacement : MonoBehaviour
             Debug.Log("Press");
             _instPreviewObj = Instantiate(_previewTower, transform.position, Quaternion.identity);//previewModel
             _instPreviewObj.transform.parent = transform;
-            //rend = _instPreviewObj.GetComponent<Renderer>();
-            //rend.sharedMaterial = _materia[1];
-            _isPlacing = true;
             currentState = State.Placing;
+            _isPlacing = true;
         }
 
     }
 
-
     public void ConfirmPlacement()
     {
+
         if (currentState == State.HoldPlacing)
         {
+            DisablePlatformCanvas();
             _isPlacing = false;
-            //rend.sharedMaterial = _materia[0];
-            Debug.Log("Touch Ended");
-            currentState = State.Default;
+
             _instObj = Instantiate(_tower, _instPreviewObj.transform.position, Quaternion.identity);
+            _instObj.transform.parent = transform.transform;
+            Debug.Log("Name: " + _instObj.name);
             Destroy(_instPreviewObj);
             _CharConfirmPlacementCanvas.SetActive(false);
 
+            currentState = State.Default;
         }
+
     }
 
     public void OnCancelPlacement()
     {
+
         _CharConfirmPlacementCanvas.SetActive(false);
-        currentState = State.Placing;
+        _instPreviewObj.SetActive(false);
+
     }
 
-    public void Hold(InputAction.CallbackContext context)
+    public void Hold()
     {
         if (currentState == State.Placing)
         {
+
             currentState = State.HoldPlacing;
-            _touchPressedAction.performed -= OnPlacing;
-            _CharConfirmPlacementCanvas.SetActive(true);
-            _CharConfirmPlacementCanvas.transform.position = _instPreviewObj.transform.position;
         }
     }
 
-    public void OnPlacing(InputAction.CallbackContext context)
+    public void OnPlacing()
     {
-
-        Debug.Log("Placing");
-        _position = _mainCam.ScreenPointToRay(Touchscreen.current.position.ReadValue());
-
-        if (Physics.Raycast(_position, out RaycastHit raycastHit))
+        if (Input.touchCount > 0)
         {
-            _instPreviewObj.transform.position = raycastHit.transform.position;
-            Debug.DrawRay(_position.origin, _position.direction * 20, Color.red);
-            Debug.Log("Touch Started");
+            Touch touch = Input.GetTouch(0);
+            Debug.Log("Placing");
+            if (_instPreviewObj != null)
+            {
+                _instPreviewObj.SetActive(true);
+                _position = _mainCam.ScreenPointToRay(touch.position);
+                rayCastHit = Physics.Raycast(_position, out RaycastHit raycastHit, maxDistance = Mathf.Infinity, _layerPlatform);
+                if (rayCastHit)
+                {
+                    _instPreviewObj.transform.position = raycastHit.transform.position;
+                    Debug.DrawRay(_position.origin, _position.direction * 20, Color.red);
+                    Hold();
+
+                }
+            }
+        }
+
+
+    }
+
+    void EnablePlatformCanvas()
+    {
+        foreach (GameObject platform in _meleePlatform)
+        {
+            canvasEnabler = platform.GetComponent<CanvasEnabler>();
+            canvasEnabler.EnableCanvas();
+        }
+    }
+
+    void DisablePlatformCanvas()
+    {
+        foreach (GameObject platform in _meleePlatform)
+        {
+            canvasEnabler = platform.GetComponent<CanvasEnabler>();
+            canvasEnabler.DisableCanvas();
         }
     }
 }
